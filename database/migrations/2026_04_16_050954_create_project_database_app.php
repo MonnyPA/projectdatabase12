d6a55f019e5fd94c0f19c39e47a2536c83d36df5
BM <monnybram@outlook.com>
2026-04-16 15:56:08 +0900

    Migration

--- database/migrations/2026_04_16_050954_create_project_database_app.php
+++ database/migrations/2026_04_16_050954_create_project_database_app.php
@@ -11,9 +11,68 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project_database_app', function (Blueprint $table) {
+        Schema::create('sows', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('nik');
+            $table->string('phone_number');
+            $table->string('birth_date');
+            $table->string('hire_date');
+            $table->string('id_akun_iepms');
+            $table->string('ser_wah');
+            $table->string('ser_first_aid');
+            $table->string('ser_electrikal');
+            $table->string('status_employee');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->foreignId('department_id')->constrained('departments');
+            $table->string('foto_ktp');
+            $table->string('foto_selfie');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('site_id')->unique();
+            $table->string('site_name');
+            $table->string('lattitude');
+            $table->string('longitude');
+            $table->foreignId('sow_id')->constrained('sows');
+            $table->date('assign_date');
+            $table->foreignId('employee_id')->constrained('employees');
+            $table->string('progress');
+            $table->date('permit_start_date');
+            $table->date('permit_end_date');
+            $table->date('start_action_date');
+            $table->date('end_action_date');
+            $table->string('foto_ba_outbound');
+            $table->string('foto_ba_inbound');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -22,6 +81,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('project_database_app');
+        Schema::dropIfExists('sows');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('departments');
+        Schema::dropIfExists('employees');
+        Schema::dropIfExists('projects');
     }
 };
